496b74f579ec0cc6182b666d89dd5541c721acaa
joanamds <joanamdsantos1@gmail.com>
2023-06-01 18:30:52 -0300

    :sparkles: feat: implementando a função study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,13 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    count = 0
+
+    # permanence_period são tuplas
+    # exemplo: [(2, 2), (1, 2)...]
+    for period in permanence_period:
+        # por isso é possível acessar os índices
+        if period[0] <= target_time <= period[1]:
+            count += 1
+
+    return count
+
+# target_time é o horário que está sendo testado

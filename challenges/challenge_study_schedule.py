fe7aab61b93be8dcbfec82135c363db3ebc09a66
joanamds <joanamdsantos1@gmail.com>
2023-06-01 18:38:43 -0300

    :sparkles: feat: adicionando None caso os parâmetros não sejam passados

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,12 +1,16 @@
 def study_schedule(permanence_period, target_time):
     count = 0
-
-    # permanence_period são tuplas
-    # exemplo: [(2, 2), (1, 2)...]
-    for period in permanence_period:
-        # por isso é possível acessar os índices
-        if period[0] <= target_time <= period[1]:
-            count += 1
+    if not permanence_period:
+        return None
+    elif not target_time:
+        return None
+    else:
+        # permanence_period são tuplas
+        # exemplo: [(2, 2), (1, 2)...]
+        for period in permanence_period:
+            # por isso é possível acessar os índices
+            if period[0] <= target_time <= period[1]:
+                count += 1
 
     return count
 

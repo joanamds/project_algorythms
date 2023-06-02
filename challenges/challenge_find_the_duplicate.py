4dd6a2e38ca8f040b53a9b149a6cfb17c3ac45fc
joanamds <joanamdsantos1@gmail.com>
2023-06-02 17:07:22 -0300

    :sparkles: feat: testando a função

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,7 @@
 def find_duplicate(nums):
+    """
+    Given a list of n + 1 integers between 1 and n, find one of the duplicates.
+    If there are multiple possible answers, return one of the duplicates.
+    If there is no duplicate, return None.
+    """
     pass
--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,9 +1,22 @@
+# def is_palindrome_iterative(word):
+#     if len(word) <= 0:
+#         return False
+#     low_index = 0
+#     high_index = len(word) - 1
+#     while low_index < high_index:
+#         if word[low_index] != word[high_index]:
+#             return False
+#         low_index += 1
+#         high_index -= 1
+#     return True
+
 def is_palindrome_iterative(word):
     if len(word) <= 0:
         return False
+    word = word.lower()  # Converter para minúsculo
     low_index = 0
     high_index = len(word) - 1
-    while low_index < high_index:
+    while low_index <= high_index:  # Modificar a condição do loop
         if word[low_index] != word[high_index]:
             return False
         low_index += 1

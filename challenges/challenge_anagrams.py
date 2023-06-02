aab525e59f16a823ee982879187b70d3ab0ce224
joanamds <joanamdsantos1@gmail.com>
2023-06-02 17:29:16 -0300

    :hammer: refactor: refatorando algumas coisas

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,45 +1,7 @@
-def merge(left, right):
-    result = []
-    i = j = 0
-
-    while i < len(left) and j < len(right):
-        if left[i] < right[j]:
-            result.append(left[i])
-            i += 1
-        else:
-            result.append(right[j])
-            j += 1
-
-    while i < len(left):
-        result.append(left[i])
-        i += 1
-
-    while j < len(right):
-        result.append(right[j])
-        j += 1
-
-    return result
-
-
-def merge_sort(string):
-    """Ordena os caracteres de uma string."""
-    if len(string) <= 1:
-        return string
-
-    middle = len(string) // 2
-    left = string[:middle]
-    right = string[middle:]
-
-    left = merge_sort(left)
-    right = merge_sort(right)
-
-    return merge(left, right)
-
-
 def is_anagram(first_string, second_string):
     """Verifica se duas strings são anagramas."""
     if not first_string or not second_string:
-        return False
+        return first_string, second_string, False
 
     if len(first_string) == 0 or len(second_string) == 0:
         return False
@@ -50,4 +12,32 @@ def is_anagram(first_string, second_string):
     first_list = merge_sort(fs)
     second_list = merge_sort(ss)
 
-    return fs == ss
+    return "".join(first_list), "".join(second_list), first_list == second_list
+
+
+def merge_sort(string):
+    if len(string) <= 1:
+        return string
+
+    middle = len(string) // 2
+    left = merge_sort(string[:middle])
+    right = merge_sort(string[middle:])
+
+    return merge(left, right)
+
+
+def merge(left, right):
+    merged = []
+    i = j = 0
+
+    while i < len(left) and j < len(right):
+        if left[i].lower() < right[j].lower():
+            merged.append(left[i])
+            i += 1
+        else:
+            merged.append(right[j])
+            j += 1
+
+    merged.extend(left[i:])
+    merged.extend(right[j:])
+    return "".join(merged)

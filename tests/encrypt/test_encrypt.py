d2c8b185a5a533943706e1083b43c21ddb407bb9
joanamds <joanamdsantos1@gmail.com>
2023-06-02 14:14:44 -0300

    :hammer: refactor: modificando os comentários

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -3,20 +3,22 @@ from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    # Verifica se key e message não possuem os tipos corretos
+    # Se key e message não possuírem os tipos corretos
+    # uma exceção deve ser lançada
     with pytest.raises(TypeError):
         encrypt_message('Message', 'key')
     with pytest.raises(TypeError):
         encrypt_message(1, 1)
 
-    # Verifica se key não é um índice positivo válido de message
+    # Se key não for um índice positivo válido de message,
+    # retorna a string message invertida
     assert encrypt_message('Message', 0) == 'egasseM'
     assert encrypt_message('Message', -1) == 'egasseM'
     assert encrypt_message('Message', 7) == 'egasseM'
     assert encrypt_message('Message', 8) == 'egasseM'
 
-    # Verifica se key é ímpar
+    # Se key for ímpar
     assert encrypt_message('Message', 1) == 'M_egasse'
 
-    # Verifica se key é par
+    # Se key for par
     assert encrypt_message('Message', 2) == 'egass_eM'

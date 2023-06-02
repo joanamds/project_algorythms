f67dcbdaabfad2dce732f727cdd0ca1d0ae3c700
joanamds <joanamdsantos1@gmail.com>
2023-06-02 13:23:57 -0300

    :fire: feat: deletando o comentário

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,21 +1,3 @@
-# def study_schedule(permanence_period, target_time):
-#     count = 0
-#     if not target_time or not permanence_period:
-#         return None
-#     # permanence_period são tuplas
-#     # exemplo: [(2, 2), (1, 2)...]
-#     for period in permanence_period:
-#         if (len(period) != 2 or
-#                 not isinstance(period[0], int) or
-#                 not isinstance(period[1], int)):
-#             return None
-#         # por isso é possível acessar os índices
-#         elif period[0] <= target_time <= period[1]:
-#             count += 1
-
-#     return count
-
-
 def study_schedule(permanence_period, target_time):
     if not target_time or not permanence_period:
         return None
@@ -31,6 +13,3 @@ def study_schedule(permanence_period, target_time):
             count += 1
 
     return count
-
-
-# target_time é o horário que está sendo testado
